8c3502dc9e062cce87ffd154c52449da6605e8a3
Lucas Rassilan Vilanova <lucasrsv1@gmail.com>
2024-03-17 04:58:21 -0300

    Visual da tab últimos comandos

---
+++ front-end/src/app/models/executedCommand.ts
@@ -0,0 +1,13 @@
+export enum CommandExecutionStatus {
+	PENDING = "list-group-item-info",
+	SUCCESSFUL = "list-group-item-success",
+	AMBIGUOUS = "list-group-item-warning",
+	ERROR = "list-group-item-danger",
+	NOT_RECOGNIZED = "list-group-item-light"
+}
+
+export interface IExecutedCommand {
+	value: string;
+	status: CommandExecutionStatus;
+	// sentAt: Date;
+}
--- front-end/src/app/pages/module/components/last-commands/last-commands.component.ts
+++ front-end/src/app/pages/module/components/last-commands/last-commands.component.ts
@@ -1,12 +1,38 @@
 import { Component } from "@angular/core";
+import { MatIcon } from "@angular/material/icon";
+import { NgFor } from "@angular/common";
+
+import { NgScrollbar } from "ngx-scrollbar";
+
+import { CommandExecutionStatus, IExecutedCommand } from "../../../../models/executedCommand";
 
 @Component({
 	selector: "app-module-last-commands",
 	standalone: true,
-	imports: [],
+	imports: [MatIcon, NgFor, NgScrollbar],
 	templateUrl: "./last-commands.component.html",
 	styleUrl: "./last-commands.component.scss"
 })
 export class LastCommandsComponent {
+	public running = CommandExecutionStatus.PENDING;
+	public commands: IExecutedCommand[] = [
+		{ value: "aumentar volume", status: CommandExecutionStatus.PENDING },
+		{ value: "tocar música porta aberta da luka", status: CommandExecutionStatus.SUCCESSFUL },
+		{ value: "tocar porta aberta", status: CommandExecutionStatus.AMBIGUOUS },
+		{ value: "teste de microfone", status: CommandExecutionStatus.NOT_RECOGNIZED },
+		{ value: "tocar a música algum nome não encontrado", status: CommandExecutionStatus.ERROR },
+		{ value: "aumentar volume", status: CommandExecutionStatus.AMBIGUOUS },
+		{ value: "tocar música porta aberta da luka", status: CommandExecutionStatus.SUCCESSFUL },
+		{ value: "tocar porta aberta", status: CommandExecutionStatus.AMBIGUOUS },
+		{ value: "teste de microfone", status: CommandExecutionStatus.NOT_RECOGNIZED },
+		{ value: "tocar a música algum nome não encontrado", status: CommandExecutionStatus.ERROR }
+	];
 
+	public edit (command: IExecutedCommand) {
+		console.log("EDIT", command);
+	}
+
+	public replay (command: IExecutedCommand) {
+		console.log("REPLAY", command);
+	}
 }
